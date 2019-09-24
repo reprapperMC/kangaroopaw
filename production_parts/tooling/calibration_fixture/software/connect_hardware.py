1036b207e9a855721dea2afbc13bf212dbd4932a
Steven Abadie <steven@alephobjects.com>
2019-09-24 10:38:21 -0600

    Add connection functions for all gauges and tools

--- production_parts/tooling/calibration_fixture/software/connect_hardware.py
+++ production_parts/tooling/calibration_fixture/software/connect_hardware.py
@@ -1,37 +1,88 @@
 import serial
 from serial.tools import list_ports
 
+
 class connectHardware():
     def __init__(self):
         self.ports = {}
 
-    def connect_DFS50(self):
-        # Need to add safety for multiple of the same gauge
-        port = list(list_ports.grep("0403:6001"))[0].device
-        self.ports['dfs50'] = serial.Serial(port, 38400)
-        if self.ports['dfs50'].is_open:
-            print("DFS50 connected")
+    # Need to add removal of a device from ports if it is disconnected.
+
+    def disconnect_all_devices(self):
+        for port in self.ports:
+            self.ports[port].close()
+        self.ports = {}
+
+    def connect_DFS_gauges(self):
+        # X and Z 'A107MY3F'
+        # Y 'A107MY3I'
+        if 'dfs_y' not in self.ports or 'dfs_xz' not in self.ports and list(list_ports.grep("0403:6001")):
+            for gauge in list(list_ports.grep("0403:6001")):
+                port = gauge.device
+                if gauge.serial_number == "A107MY3F":
+                    self.ports['dfs_xz'] = serial.Serial(port, 38400)
+                    if self.ports['dfs_xz'].is_open:
+                        print("DFS X and Z axis force gauge connected")
+                    else:
+                        print("Something went wrong attempting to connect the \
+                              DFS X and Z axis force gauge.")
+                elif gauge.serial_number == "A107MY3I":
+                    self.ports['dfs_y'] = serial.Serial(port, 38400)
+                    if self.ports['dfs_y'].is_open:
+                        print("DFS Y axis force gauge connected")
+                    else:
+                        print("Something went wrong attempting to connect the \
+                              DFS Y axis force gauge.")
+        else:
+            print("There are no DFS force gauges connected.")
 
     def connect_dial_switch(self):
-        port = list(list_ports.grep("2341:0043"))[0].device
-        self.ports['dialswitch'] = serial.Serial(port, 115200)
-        if self.ports['dialswitch'].is_open:
-            print("Dial Switch connected")
+        if 'dialswitch' not in self.ports and list(list_ports.grep("2341:0043")):
+            port = list(list_ports.grep("2341:0043"))[0].device
+            self.ports['dialswitch'] = serial.Serial(port, 115200)
+            if self.ports['dialswitch'].is_open:
+                print("Dial Switch connected")
+            else:
+                print("Sorry something failed while attempting to connect.")
+        else:
+            print("The dial gauge switch is not connected.")
     
-    def connect_actuator_controller(self):        
-        port = list(list_ports.grep("RAMBo"))[0].device
-        self.ports['actuator'] = serial.Serial(port, 120000)
-        if self.ports['actuator'].is_open:
-            print("Actuator controller connected")
+    def connect_actuator_controller(self):
+        #Actuator USB device serial number 755303131313519152D0
+        if 'actuator' not in self.ports and list(list_ports.grep("RAMBo")):
+            for controller in list(list_ports.grep("RAMBo")):
+                port = controller.device
+                if controller.serial_number == "755303131313519152D0":
+                    self.ports['actuator'] = serial.Serial(port, 120000)
+                    if self.ports['actuator'].is_open:
+                        print("Actuator controller connected")
+                    else:
+                        print("Sorry something failed while attempting to \
+                              connect.")
+        else:
+            print("The actuator controller is not connected.")
 
     def connect_printer(self):
-        if 'RAMBo' not in self.ports:
-            print("Establish the connection with the actuator controller first")
+        if 'printer' not in self.ports and list(list_ports.grep("RAMBo")):
+            for controller in list(list_ports.grep("RAMBo")):
+                port = controller.device
+                if controller.serial_number != "755303131313519152D0":
+                    self.ports['printer'] = serial.Serial(port, 120000)
+                    if self.ports['printer'].is_open:
+                        print("Printer controller connected")
+                    else:
+                        print("Sorry something failed while attempting to \
+                              connect.")
+                else:
+                    break
         else:
-            port = list(list_ports.grep("RAMBo"))[0].device
-            self.ports['printer'] = serial.Serial(port, 120000)
-            if self.ports['printer'].is_open:
-                print("Printer controller connected")
+            print("The printer is not connected")
+
+    def connect_all_gauges(self):
+        self.connect_DFS_gauges()
+        self.connect_dial_switch()
+        self.connect_actuator_controller()
+        
 
 
 
