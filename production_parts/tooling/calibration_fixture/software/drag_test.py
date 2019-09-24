70dde493d636b3b7111f7c56a78a3dd45ba723a5
Steven Abadie <steven@alephobjects.com>
2019-09-24 15:53:55 -0600

    Add draft drag test class for performing drag test on axis and collecting data

---
+++ production_parts/tooling/calibration_fixture/software/drag_test.py
@@ -0,0 +1,64 @@
+import threading
+from time import sleep
+from connect_hardware import connectHardware
+from nextech_gauge import nextechReading
+
+class dragTest():
+    def __init__(self):
+        self.nextech = nextechReading()
+    
+    def printer_xy_prep(self, devices):
+        with open('gcodes/machine_XY_drag.gcode', 'r') as gcode:
+            gcode_lines = gcode.read().splitlines()
+        for line in gcode_lines:
+            devices.ports['printer'].write((line + '\r\n').encode('utf-8'))
+
+    def printer_z_prep(self, devices):
+        with open('gcodes/machine_Z_drag.gcode', 'r') as gcode:
+            gcode_lines = gcode.read().splitlines()
+        for line in gcode_lines:
+            devices.ports['printer'].write((line + '\r\n').encode('utf-8'))
+
+    def x_drag_actuate(self, devices):
+        with open('gcodes/fixture_X_drag_calibration.gcode', 'r') as gcode:
+            gcode_lines = gcode.read().splitlines()
+        for line in gcode_lines:
+            devices.ports['actuator'].write((line + '\r\n').encode('utf-8'))
+
+    def y_drag_actuate(self, devices):
+        with open('gcodes/fixture_Y_drag_calibration.gcode', 'r') as gcode:
+            gcode_lines = gcode.read().splitlines()
+        for line in gcode_lines:
+            devices.ports['actuator'].write((line + '\r\n').encode('utf-8'))
+
+    def z_drag_actuate(self, devices):
+        with open('gcodes/fixture_Z_drag_calibration.gcode', 'r') as gcode:
+            gcode_lines = gcode.read().splitlines()
+        for line in gcode_lines:
+            devices.ports['actuator'].write((line + '\r\n').encode('utf-8'))
+
+    def x_axis_drag_test(self, devices, command):
+        self.printer_xy_prep(devices)
+        sleep(15.0)
+        reading = []
+        t = threading.Timer(4, self.x_drag_actuate, args=(devices,)).start()
+        reading_count = 100
+        while reading_count != 0:
+            reading.append(self.nextech.get_clean_force_reading(devices, 'dfs_xz', 'x'))
+            reading_count -= 1
+            sleep(0.5)
+        return reading
+
+    def y_axis_drag_test(self, devices, command):
+        self.printer_xy_prep(devices)
+        sleep(15.0)
+        reading = []
+        t = threading.Timer(6, self.y_drag_actuate, args=(devices,)).start()
+        reading_count = 100
+        while reading_count != 0:
+            reading.append(self.nextech.get_clean_force_reading(devices, 'dfs_xz', 'x'))
+            reading_count -= 1
+            sleep(0.5)
+        return reading
+            
+        
