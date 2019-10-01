3e51d6fda8d6ad4b3a828191e62769f3d9ad795e
Steven Abadie <steven@alephobjects.com>
2019-10-01 07:47:38 -0600

    Add mitutoyo dial gauge trigger and reading class

--- production_parts/tooling/calibration_fixture/software/mitutoyo_gauge.py
+++ production_parts/tooling/calibration_fixture/software/mitutoyo_gauge.py
@@ -11,6 +11,6 @@ class readDialGauge():
         serial.write((command).encode("utf-8"))
 
     def get_dial_reading(self, serial):
-        t = threading.Timer(0.1, self.hit_switch, args=(serial, 'A\r\n')).start()
+        t = threading.Timer(0.3, self.hit_switch, args=(serial, 'A\r\n')).start()
         self.read_gauge()
         return self.reading
