51003cbbcd13da81b62136cbf0204c3dca1436c3
Steven Abadie <steven@alephobjects.com>
2019-10-01 07:58:08 -0600

    Rename backlash test to accuracy test and remove development logs

--- production_parts/tooling/calibration_fixture/software/accuracy_test.py
+++ production_parts/tooling/calibration_fixture/software/accuracy_test.py
@@ -3,7 +3,7 @@ from time import sleep
 from mitutoyo_gauge import readDialGauge
 
 
-class backlashTest():
+class accuracyTest():
     def __init__(self):
         self.mt_gauge = readDialGauge()
         self.x_dial_setup = 'gcodes/x_dial_indicator_setup.gcode'
@@ -27,33 +27,29 @@ class backlashTest():
     def printer_z_prep(self, devices):
         self.run_printer_gcode(devices, self.z_dial_setup)
 
-    def run_backlash_test(self, devices, gcode):
+    def run_accuracy_test(self, devices, gcode):
         with open(gcode, 'r') as gcode:
             gcode_lines = gcode.read().splitlines()
-        print(gcode_lines)
         reading = []
         for line in gcode_lines:
             devices.ports['printer'].write(('\r\n' + line + '\r\n').encode('utf-8'))
             read = ""
             while "ok" not in str(read):
                 read = devices.ports['printer'].readline()
-            print(str(read))
             sleep(1)
             reading.append(self.mt_gauge.get_dial_reading(
                            devices.ports['dialswitch']))
-        reading.pop(0)
-        reading.pop()
         return reading
 
-    def x_axis_backlash_test(self, devices):
-        reading = self.run_backlash_test(devices, self.x_dial_calibration)
+    def x_axis_accuracy_test(self, devices):
+        reading = self.run_accuracy_test(devices, self.x_dial_calibration)
         return reading
 
-    def y_axis_backlash_test(self, devices):
-        reading = self.run_backlash_test(devices, self.y_dial_calibration)
+    def y_axis_accuracy_test(self, devices):
+        reading = self.run_accuracy_test(devices, self.y_dial_calibration)
         return reading
 
-    def z_axis_backlash_test(self, devices):
-        reading = self.run_backlash_test(devices, self.z_dial_calibration)
+    def z_axis_accuracy_test(self, devices):
+        reading = self.run_accuracy_test(devices, self.z_dial_calibration)
         return reading
 
