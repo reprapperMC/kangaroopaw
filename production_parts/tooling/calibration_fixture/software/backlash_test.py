2d62491367c4680c92645b80b2fb6de0be284dcd
Steven Abadie <steven@alephobjects.com>
2019-10-01 07:44:43 -0600

    Add backlash test, dial gauge trigger, and gcodes

---
+++ production_parts/tooling/calibration_fixture/software/backlash_test.py
@@ -0,0 +1,59 @@
+import threading
+from time import sleep
+from mitutoyo_gauge import readDialGauge
+
+
+class backlashTest():
+    def __init__(self):
+        self.mt_gauge = readDialGauge()
+        self.x_dial_setup = 'gcodes/x_dial_indicator_setup.gcode'
+        self.z_dial_setup = 'gcodes/z_dial_indicator_setup.gcode'
+        self.x_dial_calibration = 'gcodes/x_dial_indicator_calibration.gcode'
+        self.y_dial_calibration = 'gcodes/y_dial_indicator_calibration.gcode'
+        self.z_dial_calibration = 'gcodes/z_dial_indicator_calibration.gcode'
+
+    def printer_home(self, devices):
+        devices.ports['printer'].write(('\r\n' + 'G28' + '\r\n').encode('utf-8'))
+
+    def run_printer_gcode(self, devices, gcode):
+        with open(gcode, 'r') as gcode:
+            gcode_lines = gcode.read().splitlines()
+        for line in gcode_lines:
+            devices.ports['printer'].write(('\r\n' + line + '\r\n').encode('utf-8'))
+
+    def printer_x_prep(self, devices):
+        self.run_printer_gcode(devices, self.x_dial_setup)
+
+    def printer_z_prep(self, devices):
+        self.run_printer_gcode(devices, self.z_dial_setup)
+
+    def run_backlash_test(self, devices, gcode):
+        with open(gcode, 'r') as gcode:
+            gcode_lines = gcode.read().splitlines()
+        print(gcode_lines)
+        reading = []
+        for line in gcode_lines:
+            devices.ports['printer'].write(('\r\n' + line + '\r\n').encode('utf-8'))
+            read = ""
+            while "ok" not in str(read):
+                read = devices.ports['printer'].readline()
+            print(str(read))
+            sleep(1)
+            reading.append(self.mt_gauge.get_dial_reading(
+                           devices.ports['dialswitch']))
+        reading.pop(0)
+        reading.pop()
+        return reading
+
+    def x_axis_backlash_test(self, devices):
+        reading = self.run_backlash_test(devices, self.x_dial_calibration)
+        return reading
+
+    def y_axis_backlash_test(self, devices):
+        reading = self.run_backlash_test(devices, self.y_dial_calibration)
+        return reading
+
+    def z_axis_backlash_test(self, devices):
+        reading = self.run_backlash_test(devices, self.z_dial_calibration)
+        return reading
+
